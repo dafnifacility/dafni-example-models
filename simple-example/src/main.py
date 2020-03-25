b89fdc9850f562ffade0d2ad244af829f9e74aaf
Matthew Wilcoxson <matthew.wilcoxson@stfc.ac.uk>
2020-03-25 19:15:19 +0000

    Check the inputs in the simple-example.

--- simple-example/src/main.py
+++ simple-example/src/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import json
 from pathlib import Path
 from work import do_work
@@ -14,15 +15,26 @@ def main():
 
     print("Starting example model")
 
-    # Read value from environment variable using the default value
-    # if it is not specified and convert the value to an integer
-    sequence_length = int(os.getenv("SEQUENCE_LENGTH", SEQUENCE_LENGTH_DEFAULT))
-    sequence_f0 = int(os.getenv("SEQUENCE_F0", SEQUENCE_F0_DEFAULT))
-    sequence_f1 = int(os.getenv("SEQUENCE_F1", SEQUENCE_F1_DEFAULT))
+    # Read values from environment variables but use default values if they don't exist
+    sequence_length = os.getenv("SEQUENCE_LENGTH", SEQUENCE_LENGTH_DEFAULT)
+    sequence_f0 = os.getenv("SEQUENCE_F0", SEQUENCE_F0_DEFAULT)
+    sequence_f1 = os.getenv("SEQUENCE_F1", SEQUENCE_F1_DEFAULT)
+    
+    # Check each of the values are in an acceptable format for this model
+    try:
+        sequence_length = int( sequence_length )
+    except ValueError:
+        sys.exit("Error: SEQUENCE_LENGTH must be a whole number")
 
-    if sequence_length < 5:
-        raise Exception("Sequence_length is below the minimum value")
-
+    if sequence_length <= 0:
+        sys.exit("Error: SEQUENCE_LENGTH must be greater than zero")
+    
+    try:
+        sequence_f0 = int(sequence_f0)
+        sequence_f1 = int(sequence_f1)
+    except ValueError:
+        sys.exit("Error: SEQUENCE_F0 and SEQUENCE_F1 must be whole numbers")
+    
     # Call main work
     sequence = do_work(sequence_length, sequence_f0, sequence_f1)
 
@@ -30,6 +42,7 @@ def main():
     output_folder.mkdir(parents=True, exist_ok=True)
     output_file = output_folder.joinpath("sequence.json")
     output_file.write_text(json.dumps({"sequence": sequence}))
+
 
     print("Finished example model")
 
--- tiny-example/README.md
+++ tiny-example/README.md
@@ -2,19 +2,23 @@ Tiny Model Example
 ==================
 
 This is a very simplistic model showing the absolute minimum needed to run on DAFNI.
-It uses a tiny sized container and prints "Hello NAME". It's ideal for quick testing.
+It creates a very small sized model and prints "Hello $NAME".
 
-There are five files here:
+There are three files here:
 
- - _Dockerfile_ - Builds the container that will be run by DAFNI
- - _model_description.yaml_ - Details of the model needed by the DAFNI ingest system.
- - _README.md_ - This file.
+ - _[Dockerfile](./Dockerfile)_ - Builds the container that will be run by DAFNI
+ - _[model_description.yaml](./model_description.yaml)_ - Details of the model needed by the DAFNI ingest system.
+ - _README.md_ - This helpful file.
 
 You can run this example from within the "tiny-example" folder:
 
-    docker build -t tiny-example .
-    docker run tiny-example
-    
-You can adjust the way it runs with the environmental variables:
+```bash
+docker build -t tiny-example .
+docker run tiny-example
+```
 
-    docker run -e NAME=Matthew tiny-example
+You can change the output with the environment variable:
+
+```bash
+docker run -e NAME=Matthew tiny-example
+```
