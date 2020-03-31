69219523ca842c2ae290217557e0058c50a660a3
J Matthew Wilcoxson <matthew.wilcoxson@stfc.ac.uk>
2020-03-31 10:46:06 +0100

    Some Black changes

--- simple-example/src/main.py
+++ simple-example/src/main.py
@@ -20,23 +20,25 @@ def main():
     sequence_length = os.getenv("SEQUENCE_LENGTH", SEQUENCE_LENGTH_DEFAULT)
     sequence_f0 = os.getenv("SEQUENCE_F0", SEQUENCE_F0_DEFAULT)
     sequence_f1 = os.getenv("SEQUENCE_F1", SEQUENCE_F1_DEFAULT)
-    
+
     # Check each of the values are in an acceptable format for this model
-    if not is_int(sequence_length) :
+    if not is_int(sequence_length):
         sys.exit("Error: SEQUENCE_LENGTH must be a whole number")
-        
+
     sequence_length = int(sequence_length)
     if not sequence_length >= SEQUENCE_LENGTH_MINIMUM:
-        sys.exit(f"Error: SEQUENCE_LENGTH must be a minimum of {SEQUENCE_LENGTH_MINIMUM-1}")
-    
-    if not is_int(sequence_f0) :
+        sys.exit(
+            f"Error: SEQUENCE_LENGTH must be a minimum of {SEQUENCE_LENGTH_MINIMUM-1}"
+        )
+
+    if not is_int(sequence_f0):
         sys.exit("Error: SEQUENCE_F0 must be whole number")
-        
-    if not is_int(sequence_f1) :
+
+    if not is_int(sequence_f1):
         sys.exit("Error: SEQUENCE_F1 must be whole number")
-    
+
     # Call main work
-    sequence = do_work( sequence_length, int(sequence_f0), int(sequence_f1) )
+    sequence = do_work(sequence_length, int(sequence_f0), int(sequence_f1))
 
     # Output the results to a file
     output_folder.mkdir(parents=True, exist_ok=True)
