6e41c6598c786931ab4f119845cda195fdb98c63
Matthew Wilcoxson <matthew.wilcoxson@stfc.ac.uk>
2021-01-27 15:09:08 +0000

    Simplify algorithm.

--- simple-example--fibonacci-model/src/work.py
+++ simple-example--fibonacci-model/src/work.py
@@ -18,13 +18,8 @@ def fibonacci_sequence(length, f0=0, f1=1):
     Simple Fibonacci sequence generator
     """
     sequence = [f0, f1]
-    length -= 2
-    while length > 0:
-        nth = f0 + f1
-        sequence.append(nth)
-
-        f0 = f1
-        f1 = nth
-        length -= 1
+    for pos in range(0, length-2):
+        sequence.append(sequence[pos] + sequence[pos+1])
 
     return sequence
+
