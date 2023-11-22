aa5303a0945c417de1d1946b3b9cd2fd0f1f84e4
Kyle Stevenson <Kyle.Stevenson@stfc.ac.uk>
2023-11-22 09:23:31 +0000

    Remove commented out code

--- TensorflowExample/test-MNIST.py
+++ TensorflowExample/test-MNIST.py
@@ -26,10 +26,6 @@ if isDAFNI == "True":
 else:
     print("Not running within DAFNI, using run directory")
 
-#physical_devices = tf.config.experimental.list_physical_devices('GPU') # tf.config.list_physical_devices('GPU')
-#for device in physical_devices:
-#    tf.config.experimental.set_memory_growth(device, True)
-
 print(tf.__version__)
 local_dev_ps = device_lib.list_local_devices()
 for dev in local_dev_ps:
